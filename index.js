616f152537ac4f6bae22a0b6e85da1bf6ad61ded
Benjamin Horns <benhorns@gmail.com>
2020-05-09 14:45:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ var placeInLine = line.length + 1
 line.push(name)
 return(`Welcome, ${name}. You are number ${placeInLine} in line.`)
 }
+
+function nowServing (katzDeliLine) {
+  return `${katzDeliLine[0]}`
+}
