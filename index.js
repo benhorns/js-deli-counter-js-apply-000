cdfd090430d020011b1e55c5380574b80e5c2759
Benjamin Horns <benhorns@gmail.com>
2020-05-09 15:21:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function currentLine (katzDeliLine) {
   katzDeliLine.forEach((item, index) => lineStr += `${index+1}. ${item}, `
 
   // length === 0 ? 'The line is currently empty.' : lineStr += katzDeliLine.forEach(function(item,index) {index+ ':' + item})
-  return length === 0 ?  'The line is currently empty.' :  `The line is currently ${lineStr.substring(0, lineStr.length-2)}`
+  return length === 0 ?  'The line is currently empty.' :  `The line is currently: ${lineStr.substring(0, lineStr.length-2)}`
   // if(length === 0 ) {
   //   return 'The line is currently empty.'
   // } else {
