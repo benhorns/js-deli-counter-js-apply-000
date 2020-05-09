bc3889d529c91e6333f39b90832bd80c1f17ae54
Benjamin Horns <benhorns@gmail.com>
2020-05-09 14:49:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function takeANumber(line, name) {
-var placeInLine = line.length + 1
+const placeInLine = line.length + 1
 line.push(name)
 return(`Welcome, ${name}. You are number ${placeInLine} in line.`)
 }
