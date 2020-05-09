0d56544cc2cedb8d1cd8a0fc95e7981fd4f14451
Benjamin Horns <benhorns@gmail.com>
2020-05-09 14:48:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,10 +6,11 @@ return(`Welcome, ${name}. You are number ${placeInLine} in line.`)
 
 function nowServing (katzDeliLine) {
 var currentLine = katzDeliLine.length;
+var nowServing = katzDeliLine[0]
+katzDeliLine.shift()
   if(currentLine === 0) {
     return 'There is nobody waiting to be served!'
   } else {
-  return `Currently serving ${katzDeliLine[0]}.`
-  katzDeliLine.shift();
+  return `Currently serving ${nowServing}.`
   }
 }
