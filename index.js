9f184ea14ec3073980a58fef1f771c5b6a4fd8c4
Benjamin Horns <benhorns@gmail.com>
2020-05-09 15:11:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,11 +21,11 @@ function currentLine (katzDeliLine) {
 
   // length === 0 ? 'The line is currently empty.' : lineStr += katzDeliLine.forEach(function(item,index) {index+ ':' + item})
   length === 0 ? return 'The line is currently empty.' : return `this`
-  if(length === 0 ) {
-    return 'The line is currently empty.'
-  } else {
-  katzDeliLine.forEach((item,index) => lineStr += `${index+1}. ${item}, `)
-    }
-
-  return `The line is currently: ${lineStr.substring(0, lineStr.length-2)}`
+  // if(length === 0 ) {
+  //   return 'The line is currently empty.'
+  // } else {
+  // katzDeliLine.forEach((item,index) => lineStr += `${index+1}. ${item}, `)
+  //   }
+  //
+  // return `The line is currently: ${lineStr.substring(0, lineStr.length-2)}`
 }
